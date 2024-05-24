13ea909cae2b72971ffa2f5d53aa549303708c78
Franco Ortega <francopdx@gmail.com>
2024-05-24 15:44:44 -0700

    remove unused styles and variables

--- app/components/space/Space.tsx
+++ app/components/space/Space.tsx
@@ -3,17 +3,6 @@ import styles from './Space.module.css';
 
 type Props = { children: ReactNode };
 
-// background-image: radial-gradient(
-//   1px 1px at 25px 5px,
-//   var(--white-one),
-//   var(--white-transparent)
-// ),
-
-const radGrad1 =
-  'radial-gradient(1px 1px at 25px 5px, var(--white-one), var(--white-transparent))';
-const radGrad2 =
-  'radial-gradient(1px 1px at 50px 25px, var(--white-one), var(--white-transparent))';
-
 const radGradList = [];
 
 do {
