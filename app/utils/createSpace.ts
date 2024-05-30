7150f10a0f925f22482e4094e8a1a1f68ad8215c
Franco Ortega <francopdx@gmail.com>
2024-05-30 12:11:21 -0700

    change do while loop to while loop

--- app/utils/createSpace.ts
+++ app/utils/createSpace.ts
@@ -3,11 +3,9 @@ import createStar from "./createStar";
 export default function createSpace(stars: number = 100) {
   const starList = [];
 
-do {
+  while (starList.length < stars) {
 	starList.push(createStar());
-} while (starList.length < stars);
-
-console.log(starList.length)
+};
 
 const space = starList.reduce((a, c) => a + ', ' + c);
 
