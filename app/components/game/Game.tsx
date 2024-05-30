85ab63e60867d410ce30fb7d1fa20d2382ef6d39
Franco Ortega <francopdx@gmail.com>
2024-05-30 16:20:31 -0700

    create SPACE GAME header

--- app/components/game/Game.tsx
+++ app/components/game/Game.tsx
@@ -3,7 +3,11 @@ import styles from './Game.module.css';
 type Props = {};
 
 function Game({}: Props) {
-	return <main className={styles.Game}>Game</main>;
+	return (
+		<main className={styles.Game}>
+			<h1>SPACE GAME</h1>
+		</main>
+	);
 }
 
 export default Game;
