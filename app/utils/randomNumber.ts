e8d31352048d9192fb3648eb1b518f37788597f0
Franco Ortega <francopdx@gmail.com>
2024-05-30 11:47:36 -0700

    remove default values for randomInteger as they are already handled by randomNumber

--- app/utils/randomNumber.ts
+++ app/utils/randomNumber.ts
@@ -2,6 +2,6 @@ export function randomNumber(min: number = 1, max: number = 100) {
   return (Math.random() * max) + min;
 };
 
-export function randomInteger(min: number = 1, max: number = 100) {
+export function randomInteger(min: number, max: number) {
   return Math.floor(randomNumber(min, max));
 };
