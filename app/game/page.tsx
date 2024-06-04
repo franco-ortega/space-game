845c3f86b917d445fc0bed4bcd215e16e9a96b82
Franco Ortega <francopdx@gmail.com>
2024-06-04 14:58:20 -0700

    remove GameGrid from game page

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,11 +1,9 @@
 import Game from '../components/game/Game';
-import GameGrid from '../components/game/GameGrid';
 
 export default function GamePage() {
 	return (
 		<>
-			<GameGrid />
-			{/* <Game /> */}
+			<Game />
 		</>
 	);
 }
