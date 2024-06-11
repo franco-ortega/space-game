5d00ae8f1f42f04a541de78b94a07d36fc035544
Franco Ortega <francopdx@gmail.com>
2024-06-11 14:57:31 -0700

    clean up Planet component

--- app/components/planets/Planet.tsx
+++ app/components/planets/Planet.tsx
@@ -9,6 +9,21 @@ type Props = {
 	};
 };
 
+export default function Planet({ planetDetails }: Props) {
+	const { name, color, rings } = planetDetails;
+
+	const planet = (
+		<div className={styles.Planet} style={{ backgroundColor: color }}>
+			<div>{name.toUpperCase()}</div>
+		</div>
+	);
+
+	const completePlanet = rings ? addRingsToPlanet(planet, rings) : planet;
+
+	return <>{completePlanet}</>;
+}
+
+/* HELPER FUNCTION */
 function addRingsToPlanet(planet: React.JSX.Element, amount: number) {
 	let count = 0;
 	let ringedPlanet = planet;
@@ -20,73 +35,3 @@ function addRingsToPlanet(planet: React.JSX.Element, amount: number) {
 
 	return ringedPlanet;
 }
-
-export default function Planet({ planetDetails }: Props) {
-	const { name, color, rings } = planetDetails;
-	const planetName = name.toUpperCase();
-
-	const planetBody = (
-		<div className={styles.Planet} style={{ backgroundColor: color }}>
-			<div>{planetName}</div>
-		</div>
-	);
-
-	const planet = rings ? addRingsToPlanet(planetBody, rings) : planetBody;
-
-	return <>{planet}</>;
-}
-
-// function addRingsToPlanet(planet: React.JSX.Element, amount: number) {
-// 	let count = 0;
-// 	let ringedPlanet = planet;
-
-// 	while (count < amount) {
-// 		ringedPlanet = <Ring>{ringedPlanet}</Ring>;
-// 		count++;
-// 	}
-
-// 	return ringedPlanet;
-// }
-
-// import React, { ReactNode, useState } from 'react';
-// import { GridLocation } from '@/app/types/types';
-// import Ring from '../rings/Ring';
-// import styles from './Planet.module.css';
-
-// type Props = {
-// 	name: string;
-// 	color: string;
-// 	gridLocation: GridLocation;
-// 	rings: number;
-// };
-
-// function addRingsToPlanet(planet: React.JSX.Element, amount: number) {
-// 	let count = 0;
-// 	let ringedPlanet = planet;
-
-// 	while (count < amount) {
-// 		ringedPlanet = <Ring>{ringedPlanet}</Ring>;
-// 		count++;
-// 	}
-
-// 	return ringedPlanet;
-// }
-
-// export default function Planet({ name, color, gridLocation, rings }: Props) {
-// 	const planetName = name.toUpperCase();
-// 	const { column, row } = gridLocation;
-
-// 	const planetBody = (
-// 		<div className={styles.PlanetBody} style={{ backgroundColor: color }}>
-// 			<div className={styles.PlanetName}>{planetName}</div>
-// 		</div>
-// 	);
-
-// 	const planet = rings ? addRingsToPlanet(planetBody, rings) : planetBody;
-
-// 	return (
-// 		<div className={styles.Planet} style={{ gridColumn: column, gridRow: row }}>
-// 			{planet}
-// 		</div>
-// 	);
-// }
