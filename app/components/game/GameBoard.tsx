6e8486a6bf4de658142b77e87a7759cb4a425296
Franco Ortega <francopdx@gmail.com>
2024-06-11 14:28:22 -0700

    update all planets to use PlanetContainer

--- app/components/game/GameBoard.tsx
+++ app/components/game/GameBoard.tsx
@@ -7,7 +7,56 @@ type Props = {};
 export default function GameBoard({}: Props) {
 	return (
 		<main className={styles.GameBoard}>
-			<Planet
+			<PlanetContainer
+				planetData={{
+					name: 'JOA',
+					color: 'blue',
+					coordinates: {
+						column: '3 / 8',
+						row: '3 / 8',
+					},
+					rings: 2,
+				}}
+			/>
+			<PlanetContainer
+				planetData={{
+					name: 'KOA',
+					color: 'lightblue',
+					coordinates: {
+						column: '6 / 14',
+						row: '10 / 18',
+					},
+					rings: 4,
+				}}
+			/>
+			<PlanetContainer
+				planetData={{
+					name: 'IOA',
+					color: 'darkred',
+					coordinates: {
+						column: '15 / 19',
+						row: '5 / 9',
+					},
+					rings: 0,
+				}}
+			/>
+			<PlanetContainer
+				planetData={{
+					name: 'ZOA',
+					color: 'orange',
+					coordinates: {
+						column: '13 / 16',
+						row: '17 / 20',
+					},
+					rings: 0,
+				}}
+			/>
+		</main>
+	);
+}
+
+{
+	/* <Planet
 				name={'JOA'}
 				color={'blue'}
 				gridLocation={{
@@ -33,18 +82,5 @@ export default function GameBoard({}: Props) {
 					row: '3 / 9',
 				}}
 				rings={0}
-			/>
-			<PlanetContainer
-				planetData={{
-					name: 'ZOA',
-					color: 'orange',
-					coordinates: {
-						column: '16 / 23',
-						row: '3 / 9',
-					},
-					rings: 0,
-				}}
-			/>
-		</main>
-	);
+			/> */
 }
