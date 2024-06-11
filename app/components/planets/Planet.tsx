7536419bd88dfd605367c2152b194e91d3252643
Franco Ortega <francopdx@gmail.com>
2024-06-11 11:40:39 -0700

    make the amount of rings be dynamic

--- app/components/planets/Planet.tsx
+++ app/components/planets/Planet.tsx
@@ -1,4 +1,6 @@
-import React, { ReactNode } from 'react';
+'use client';
+
+import React, { ReactNode, useState } from 'react';
 import { GridLocation } from '@/app/types/types';
 import Ring from '../rings/Ring';
 import styles from './Planet.module.css';
@@ -13,6 +15,7 @@ type Props = {
 export default function Planet({ name, color, gridLocation, ring }: Props) {
 	const planetName = name.toUpperCase();
 	const { column, row } = gridLocation;
+	// const [count, setCount] = useState(0);
 
 	const planetBody = (
 		<div className={styles.PlanetBody} style={{ backgroundColor: color }}>
@@ -20,15 +23,35 @@ export default function Planet({ name, color, gridLocation, ring }: Props) {
 		</div>
 	);
 
+	// function ringIt(item: React.JSX.Element) {
+	// 	console.log(count);
+	// 	setCount((prev) => prev + 1);
+	// 	console.log('ring it');
+	// 	return <Ring>{item}</Ring>;
+	// }
+
+	const ringNumber = 2;
+	let count = 0;
+	let ringedPlanet: React.JSX.Element = planetBody;
+
+	while (count < ringNumber) {
+		// console.log('test');
+
+		ringedPlanet = <Ring>{ringedPlanet}</Ring>;
+		count++;
+		// ringIt(ringedPlanet);
+	}
+
+	console.log(count);
+
 	return (
 		<div className={styles.Planet} style={{ gridColumn: column, gridRow: row }}>
-			{ring ? (
-				<Ring>
-					<Ring>{planetBody}</Ring>
-				</Ring>
-			) : (
-				planetBody
-			)}
+			{ring
+				? // <Ring>
+				  // 	<Ring>{planetBody}</Ring>
+				  // </Ring>
+				  ringedPlanet
+				: planetBody}
 		</div>
 	);
 }
