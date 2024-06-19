134b9d95e07d7eabc612238f73ace9d63237c841
Franco Ortega <francopdx@gmail.com>
2024-06-19 11:07:46 -0700

    destructure and apply zone coordinates to ZoneTemplate

--- app/components/zones/ZoneTemplate.tsx
+++ app/components/zones/ZoneTemplate.tsx
@@ -1,9 +1,23 @@
+import { ZoneData } from '@/app/types/types';
 import styles from './ZoneTemplate.module.css';
 
 type Props = {
 	children: React.ReactNode;
+	zoneData: ZoneData;
 };
 
-export default function ZoneTemplate({ children }: Props) {
-	return <div className={styles.ZoneTemplate}>{children}</div>;
+export default function ZoneTemplate({ children, zoneData }: Props) {
+	const { zoneCoordinates } = zoneData;
+
+	return (
+		<div
+			className={styles.ZoneTemplate}
+			style={{
+				gridColumn: zoneCoordinates.column,
+				gridRow: zoneCoordinates.row,
+			}}
+		>
+			{children}
+		</div>
+	);
 }
