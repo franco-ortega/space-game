ce8eeb21eed6536bb715c58b1a5a35375b2a81ad
Franco Ortega <francopdx@gmail.com>
2024-06-20 13:01:10 -0700

    create Zone4 - starting empty

---
+++ app/components/zones/Zone4.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ZoneTemplate from './ZoneTemplate';
+import PlanetContainer from '../planets/PlanetContainer';
+
+type Props = {};
+
+export default function Zone4({}: Props) {
+	return (
+		<ZoneTemplate
+			zoneData={{
+				zoneCoordinates: {
+					column: '16 / 30',
+					row: '11 / 17',
+				},
+			}}
+		>
+			Zone 4
+		</ZoneTemplate>
+	);
+}
