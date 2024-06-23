f4475fcaebcf6a40f8b5a9dc69665c6533103c64
Franco Ortega <francopdx@gmail.com>
2024-06-23 12:30:30 -0700

    remove ring logic from Planet

--- app/components/planets/Planet.tsx
+++ app/components/planets/Planet.tsx
@@ -1,5 +1,4 @@
 import { PlanetData } from '@/app/types/types';
-import Ring from '../rings/Ring';
 import styles from './Planet.module.css';
 
 type Props = {
@@ -7,28 +6,24 @@ type Props = {
 };
 
 export default function Planet({ planetData }: Props) {
-	const { name, color, rings, width } = planetData;
+	const { name, color, width } = planetData;
 
-	const planet = (
+	return (
 		<div className={styles.Planet} style={{ backgroundColor: color, width }}>
 			<div>{name.toUpperCase()}</div>
 		</div>
 	);
-
-	const completePlanet = rings ? addRingsToPlanet(planet, rings) : planet;
-
-	return <>{completePlanet}</>;
 }
 
 /* HELPER FUNCTION */
-function addRingsToPlanet(planet: React.JSX.Element, amount: number) {
-	let count = 0;
-	let ringedPlanet = planet;
+// function addRingsToPlanet(planet: React.JSX.Element, amount: number) {
+// 	let count = 0;
+// 	let ringedPlanet = planet;
 
-	while (count < amount) {
-		ringedPlanet = <Ring>{ringedPlanet}</Ring>;
-		count++;
-	}
+// 	while (count < amount) {
+// 		ringedPlanet = <Ring>{ringedPlanet}</Ring>;
+// 		count++;
+// 	}
 
-	return ringedPlanet;
-}
+// 	return ringedPlanet;
+// }
