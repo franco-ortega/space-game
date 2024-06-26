3dd75e38dd86eb467883972d56164bbe339ca379
Franco Ortega <francopdx@gmail.com>
2024-06-26 13:43:18 -0700

    convert Ring color, width, and padding into prop that will use the style tag

--- app/components/rings/Ring.tsx
+++ app/components/rings/Ring.tsx
@@ -2,9 +2,28 @@ import { ReactNode } from 'react';
 import styles from './Ring.module.css';
 
 type Props = {
+	ringWidth: string;
+	ringColor: string;
+	ringPadding: string;
 	children: ReactNode;
 };
 
-export default function Ring({ children }: Props) {
-	return <div className={styles.Ring}>{children}</div>;
+export default function Ring({
+	ringColor = 'yellow',
+	ringPadding = '5px',
+	ringWidth = '2px',
+	children,
+}: Props) {
+	return (
+		<div
+			className={styles.Ring}
+			style={{
+				borderColor: ringColor,
+				borderWidth: ringWidth,
+				padding: ringPadding,
+			}}
+		>
+			{children}
+		</div>
+	);
 }
