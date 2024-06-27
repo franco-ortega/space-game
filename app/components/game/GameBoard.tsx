c6a2f23bab3d96fd607a404a7dc9f1b5d434e1f0
Franco Ortega <francopdx@gmail.com>
2024-06-26 17:09:17 -0700

    move helper function to create planet into utils folder

--- app/components/game/GameBoard.tsx
+++ app/components/game/GameBoard.tsx
@@ -1,14 +1,23 @@
 import planets from '../../data/planets.json';
 import zones from '../../data/zones.json';
+import createPlanet from '@/app/utils/createPlanet';
 import Zone from '../zones/Zone';
 import SpaceDock from '../spaceDock/SpaceDock';
-import Orbit from '../orbit/Orbit';
-import Ring from '../rings/Ring';
-import Planet from '../planets/Planet';
 import styles from './GameBoard.module.css';
-import { PlanetData } from '@/app/types/types';
 
 export default function GameBoard() {
+	const { doa, ioa, joa, koa, zoa } = planets;
+
+	const PlanetJoa = createPlanet(joa);
+	const PlanetIoa = createPlanet(ioa);
+	const PlanetDoa = createPlanet(doa);
+	const PlanetKoa = createPlanet(koa);
+	const PlanetZoa = createPlanet(zoa);
+
+	function EmptyZone() {
+		return <></>;
+	}
+
 	return (
 		<main className={styles.GameBoard}>
 			<SpaceDock />
@@ -63,98 +72,3 @@ export default function GameBoard() {
 		</main>
 	);
 }
-
-/*
-PLANETS
-
-a PLANET is comprised of:
-- ORBIT parent component
-- PLANET child component 
-*/
-
-const { doa, ioa, joa, koa, zoa } = planets;
-
-function createPlanet(planetData: PlanetData) {
-	return function CreatePlanet() {
-		return (
-			<Orbit planetPosition={planetData.orbit}>
-				<Planet planetData={planetData} />
-			</Orbit>
-		);
-	};
-}
-
-const PlanetJoa = createPlanet(joa);
-const PlanetIoa = createPlanet(ioa);
-const PlanetDoa = createPlanet(doa);
-const PlanetKoa = createPlanet(koa);
-const PlanetZoa = createPlanet(zoa);
-
-// function PlanetJoa() {
-// 	return (
-// 		<Orbit planetPosition={joa.orbit}>
-// 			<Planet planetData={joa} />
-// 		</Orbit>
-// 	);
-// }
-
-// function PlanetIoa() {
-// 	return (
-// 		<Orbit planetPosition={ioa.orbit}>
-// 			<Planet planetData={ioa} />
-// 		</Orbit>
-// 	);
-// }
-
-// function PlanetKoa() {
-// 	return (
-// 		<Orbit planetPosition={koa.orbit}>
-// 			<Planet planetData={koa} />
-// 		</Orbit>
-// 	);
-// }
-
-// function PlanetZoa() {
-// 	return (
-// 		<Orbit planetPosition={zoa.orbit}>
-// 			<Planet planetData={zoa} />
-// 		</Orbit>
-// 	);
-// }
-
-// function PlanetDoa() {
-// 	return (
-// 		<Orbit planetPosition={doa.orbit}>
-// 			<Planet planetData={doa} />
-// 		</Orbit>
-// 	);
-// }
-
-function EmptyZone() {
-	return <></>;
-}
-
-/* Test planet and previous architecture of existing planets */
-
-// function PlanetTest() {
-// 	return (
-// 		<Orbit
-// 			planetPosition={{
-// 				alignItems: 'flex-start',
-// 				justifyContent: 'flex-start',
-// 			}}
-// 		>
-// 			<Ring>
-// 				<Ring>
-// 					<Planet
-// 						planetData={{
-// 							name: 'TEST',
-// 							color: 'green'
-// 							width: '5rem',
-// 						}}
-// 					/>
-// 				</Ring>
-// 			</Ring>
-// 		</Orbit>
-// 	);
-// }
